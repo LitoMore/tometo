f2340abce1185af12a4f430d6935b1d733cedb1b
LitoMore <litomore@gmail.com>
2018-09-11 14:58:41 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # tometo
+
 A CLI tomato timer tool with system notifier
+
+## Install
+
+```bash
+$ npm i -g tometo
+```
+
+## Usage
+
+```bash
+$ tometo [minutes] &
+```
+
+## License
+
+MIT @ [LitoMore](https://github.com/LitoMore)
---
+++ cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+'use strict';
+
+const meow = require('meow');
+const notifier = require('node-notifier');
+
+const cli = meow();
+
+const [time = 25] = cli.input;
+
+if (isNaN(time)) {
+	console.log('Invalid time input');
+	process.exit(1);
+}
+
+setTimeout(() => {
+	notifier.notify({
+		title: 'Tometo',
+		message: 'Time\'s up'
+	});
+}, time * 1000 * 60);
